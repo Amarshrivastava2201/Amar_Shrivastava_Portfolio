7a0a9ec46f9d7ce6b0f7f50d2636497823ff07f9
Amar Shrivastava <amar.ssgi@gmail.com>
2026-03-31 03:15:12 +0530

    resolving issue in deployment

--- frontend/src/pages/Projects.jsx
+++ frontend/src/pages/Projects.jsx
@@ -1,7 +1,7 @@
 
 import { useEffect, useState } from 'react'
 import { fetchProjects } from '../services/projectService'
-import ProjectCard from '../components/projects/projectCard'
+import ProjectCard from '../components/projects/projectCard.jsx'
 
 const Projects = () => {
   const [projects, setProjects] = useState([])
