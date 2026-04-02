797c9612c0827be367007bab7c03c5a3cdf6a742
Amar Shrivastava <amar.ssgi@gmail.com>
2026-04-02 18:56:27 +0530

    resolved api calling issue in frontend

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,6 +1,6 @@
 import api from './api'
 
 export const loginAdmin = async (credentials) => {
-  const response = await api.post('/auth/login', credentials)
+  const response = await api.post(`${import.meta.env.VITE_API_URL}/auth/login`, credentials)
   return response.data
 }
